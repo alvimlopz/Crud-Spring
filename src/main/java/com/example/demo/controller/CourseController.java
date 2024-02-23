c7574996f20c95dbfebc56f67346ebd1aa42b041
alvaro rodrigues <alvim.lopez@gmail.com>
2024-02-23 10:56:22 -0300

    Mapeando a entidade

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -1,5 +1,16 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/course")
 public class CourseController {
 
+	
+	public List<Object> list(){
+		return null;
+	}
 }
---
+++ src/main/java/com/example/demo/models/Course.java
@@ -0,0 +1,20 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+//@Table(name = "cursos") //caso queira que o nome da tabela seja diferente da entidade
+public class Course {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String category;
+
+}
